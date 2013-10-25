1621f113729fa8d628b213291f8bf96eeb4297ee
sundawei <sundawei@gmail.com>
2013-10-25 16:03:06 +0800

    remove MaxEntropy

--- pir.cpp
+++ pir.cpp
@@ -343,62 +343,6 @@ void* sendimg(void * param)//send captured img
 	return 0;
 }
 
-int HistogramBins = 256;
-float HistogramRange1[2] = {0,255};
-float *HistogramRange[1] = {&HistogramRange1[0]};
-typedef enum {back,object} entropy_state;
-double caculateCurrentEntropy(CvHistogram * Histogram1, int cur_threshold, entropy_state state)
-{
-	int start,end;
-	if(state == back) 
-	{
-		start = 0;
-		end = cur_threshold; 
-	}
-	else
-	{
-		start = cur_threshold;
-		end = 256;
-	}
-	int  total = 0;
-	for(int i = start; i < end; i++) 
-	{
-		total += (int)cvQueryHistValue_1D(Histogram1, i);
-	}
-	double cur_entropy = 0.0;
-	for(int i = start; i < end; i++)
-	{
-		if((int)cvQueryHistValue_1D(Histogram1,i) == 0)
-			continue;
-		double percentage = cvQueryHistValue_1D(Histogram1, i) / total;
-		cur_entropy += - percentage * logf(percentage); // 能量的定义公式
-	}
-	return cur_entropy;
-}
-// 寻找最大熵阈值并分割
-void MaxEntropy(IplImage *src, IplImage *dst)
-{
-	assert(src != NULL);
-	assert(src->depth == 8 && dst->depth == 8);
-	assert(src->nChannels == 1);
-	CvHistogram * hist  = cvCreateHist(1, &HistogramBins, CV_HIST_ARRAY, HistogramRange);
-	cvCalcHist(&src, hist);
-	double maxentropy = -1.0;
-	int max_index = -1;
-	// 循环测试每个分割点，寻找到最大的阈值分割点
-	for(int i = 0; i < HistogramBins; i++)
-	{
-		double cur_entropy = caculateCurrentEntropy(hist, i, object) + caculateCurrentEntropy(hist, i, back);
-		if(cur_entropy > maxentropy)
-		{
-			maxentropy = cur_entropy;
-			max_index = i;
-		}
-	}
-	cvThreshold(src, dst, (double)max_index, 255, CV_THRESH_BINARY);
-	cvReleaseHist(&hist);
-}
-
 int main(int argc, char** argv) {
 
 
